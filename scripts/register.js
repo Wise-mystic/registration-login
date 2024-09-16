d1a1de931590e9d6a50c699fe47e02b527985ab5
Wisdom Kwawuvi <wisdomkwawuvi23@gmail.com>
2024-09-16 11:39:06 +0000

    js@1

---
+++ scripts/register.js
@@ -0,0 +1,10 @@
+// select form by Id
+const registerForm = document.getElementById('register-form');  // an id is tageted and then selected ...target the id in the html//
+// Handle form submit event
+registerForm.onsubmit = function () {
+    // collect username
+    // collect email
+    // collect password
+    // save user information
+    // send confirmation email
+} //when you submit what will happen ..a function should be invoke to get the work done, knowing what you need to//
